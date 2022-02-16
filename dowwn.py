e58d263de3a19cd1aa21aa08792ea21fe6aa6bee
rezabakh1221 <99674430+rezabakh1221@users.noreply.github.com>
2022-02-16 11:47:13 +0330

    Update dowwn.py

--- dowwn.py
+++ dowwn.py
@@ -119,9 +119,9 @@ def check_number(number,s_base):
 def change_base(client,message):
     text=str(message.text)
     text=text.split()
-    s_base=int(text[0])
-    d_base=int(text[1])
-    number=int(text[2])
+    s_base=int(text[0],base=10)
+    d_base=int(text[1],base=10)
+    number=int(text[2],base=10)
     if(check_number(number,s_base) ==1) and (s_base<=10 and d_base<=10):
         if s_base>d_base:
             result=base_number_btok(number,d_base)
